6d726b075298ce65a9ac9165c4db71281e5188a3
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2024-02-24 21:11:00 -0400

    [NEW] Adding main page content

---
+++ main.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>PHP Community</title>
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/mainpage.css">
+  </head>
+  <body>
+    <nav class="row">
+      <div class="row">
+        <img src="./assets/images/php.png" alt="PHP logo">
+        <div class="row text-links">
+          <a target="_blank" href="pixel.php">Home</a>
+          <a target="_blank" href="pixel.php">What's Php?</a>
+          <a target="_blank" href="pixel.php">Documentation</a>
+          <a target="_blank" href="pixel.php">Help</a>
+          <a target="_blank" href="pixel.php">Get Inoved</a>
+          <a target="_blank" href="pixel.php">Get Inovd</a>
+        </div>
+      </div>
+      <div class="row button-links">
+        <a class="nav-button" href="login.php">Community</a>
+        <a class="nav-button" target="_blank" href="pixel.php">Download</a>
+      </div>
+    </nav>
+
+    <main>
+      <section id="introduction">
+        <div class="column" id="main-description">
+          <h1>Explore a new world<br/>of possibilities with<br/>PHP</h1>
+          <a>Get in a touch</a>
+        </div>
+
+        <img id="arveres" src="./assets/images/vegetation.png" alt="arveres">
+      </section>
+
+      <section id="examples">
+        <div class="row" id="example-showcase">
+          <div class="column">
+            <img src="./assets/images/code-icon.png" alt="code icon">
+            <p>Lorem ipsum dolor sit amet consectetur.<br/>Vitae nam commodo pulvinar et proin.<br/>Nisi lorem facilisis semper eu nulla at<br/>commodo placerat. Eu velit pulvinar<br/>tristique ultrices pharetra.</p>
+            <a href="pixel.php" target="_blank">
+              See show case
+              <img src="./assets/images/open-icon.png" alt="see show case">
+            </a>
+          </div>
+          <div class="column">
+            <img src="./assets/images/code-icon.png" alt="code icon">
+            <p>Lorem ipsum dolor sit amet consectetur.<br/>Vitae nam commodo pulvinar et proin.<br/>Nisi lorem facilisis semper eu nulla at<br/>commodo placerat. Eu velit pulvinar<br/>tristique ultrices pharetra.</p>
+            <a href="pixel.php" target="_blank">
+              See show case
+              <img src="./assets/images/open-icon.png" alt="see show case">
+            </a>
+          </div>
+          <div class="column">
+            <img src="./assets/images/code-icon.png" alt="code icon">
+            <p>Lorem ipsum dolor sit amet consectetur.<br/>Vitae nam commodo pulvinar et proin.<br/>Nisi lorem facilisis semper eu nulla at<br/>commodo placerat. Eu velit pulvinar<br/>tristique ultrices pharetra.</p>
+            <a href="pixel.php" target="_blank">
+              See show case
+              <img src="./assets/images/open-icon.png" alt="see show case">
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  </body>
+</html>
